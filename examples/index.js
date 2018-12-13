ea58fa2986b35f08c6f05fd21cb0a87438ab072f
Ricardo Zylbergeld <ricardo@digitao.com.br>
2018-12-13 20:17:23 -0200

    Restructured tree (lib format)

---
+++ examples/index.js
@@ -0,0 +1,13 @@
+const holoGQLContainer = require('../')
+
+const schema = require('./schema');
+const setup = require('./setup.json');
+
+(async ()=>{
+    const { server, hApps } = await holoGQLContainer( schema, setup )
+
+    //pre fill with some sandbox data - for testing purposes
+    require('./sandbox_data')( hApps )
+
+    console.log(`🚀 Server ready at http://localhost:${setup.port}${server.graphqlPath}`)
+})()
--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const holoGQLContainer = require('./holoGQLContainer')
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const Container = require('@holochain/holochain-nodejs');
 
-const schema = require('./schema');
-const setup = require('./setup.json');
+module.exports = async ( schema, setup ) => {
 
-(async ()=>{
-    const { server, hApps } = await holoGQLContainer( schema, setup )
+  // instantiate and start hApps
+  const hApps = {}
+  for( key in setup.happs ) {
+    hApps[key] = Container.instanceFromNameAndDna(key, setup.happs[key] )
+    hApps[key].start()    
+  }
 
-    //pre fill with some sandbox data - for testing purposes
-    require('./sandbox_data')( hApps )
+  const server = new ApolloServer( schema(hApps) );
 
-    console.log(`🚀 Server ready at http://localhost:${setup.port}${server.graphqlPath}`)
-})()
+  const app = express();
+  server.applyMiddleware({ app });
+  
+  await app.listen({ port: setup.port || 4400 });
+  return { app, server, hApps };
+}
